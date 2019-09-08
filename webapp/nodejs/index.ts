47908c5063d7670a8ace105aa0bb91027e535325
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-09-08 14:55:46 +0900

    campaign: 2

--- webapp/nodejs/index.ts
+++ webapp/nodejs/index.ts
@@ -309,7 +309,7 @@ async function postInitialize(req: FastifyRequest, reply: FastifyReply<ServerRes
 
     const res = {
         // キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-        campaign: 1,
+        campaign: 2,
         // 実装言語を返す
         language: "nodejs",
     };
